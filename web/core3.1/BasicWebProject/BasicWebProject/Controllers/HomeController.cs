a8e823351c72aaad8067ad9bc984b74792132ba4
iseiryu <iseiryu@gmail.com>
2020-07-30 12:48:51 -0400

    Initial commit with a very basic web project.

---
+++ web/core3.1/BasicWebProject/BasicWebProject/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicWebProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "It Works!";
+    }
+}
